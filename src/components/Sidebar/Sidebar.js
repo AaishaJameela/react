2e25af7f3e0d3a8b0e9807898001b268a54da180
AaishaJameela <109809658+AaishaJameela@users.noreply.github.com>
2022-08-10 10:13:27 +0530

    Add files via upload

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import './Sidebar.css';
 
+function getSidebar() {
+    return (
+      <div className="Sidebar"><h1>Sidebar</h1>
+      <button>Home</button><br></br>
+      <button>About</button><br></br>
+      <button>Contact Us</button><br></br>
+      </div>
+      
+    )
+  }
+  
+  export default getSidebar
