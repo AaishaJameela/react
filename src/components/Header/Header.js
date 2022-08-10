3f32d091ee64047a0e2769afc737c1853a6c7e40
AaishaJameela <109809658+AaishaJameela@users.noreply.github.com>
2022-08-10 10:06:44 +0530

    Add files via upload

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import './Header.css';
 
+const getHeader = () => {
+  return (
+    <div className="Header">
+      <h1>Header</h1>
+    </div>
+  )
+}
+
+export default getHeader
