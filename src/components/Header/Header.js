b8a2921826bd18895c8d5f066b7e627c686c5236
AaishaJameela <109809658+AaishaJameela@users.noreply.github.com>
2022-08-16 15:06:29 +0530

    Add files via upload

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Header.css';
 
-const getHeader = () => {
+const getHeader = (titleName) => {
+
   return (
     <div className="Header">
-      <h1>Header</h1>
+      <h1>Header/</h1>
+      <h2 style={{ color: "blue" }}>{titleName.newTitle}</h2>
+      <h2 style={{ color: "green" }}>{titleName.newTitle1}</h2>
+      <h2 style={{ color: "violet" }}>{titleName.newTitle2}</h2>
     </div>
   )
 }
