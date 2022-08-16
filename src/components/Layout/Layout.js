f08fb3b7b9f25b4fc32995576fedb1e3fd30f213
AaishaJameela <109809658+AaishaJameela@users.noreply.github.com>
2022-08-16 15:07:21 +0530

    Add files via upload

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Header from '../Header/Header.js';
+import './Layout.css';
 import Footer from '../Footer/Footer.js';
 import Sidebar from '../Sidebar/Sidebar.js';
 
 const Layout = ({children}) => {
      return (
            <div className="Layout">
-           <Header />
            <Sidebar />
            {children}
            <Footer />
