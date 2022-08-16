1fbd94cc8f963e338998bd7323d18ef49e4c13d3
AaishaJameela <109809658+AaishaJameela@users.noreply.github.com>
2022-08-16 15:09:26 +0530

    Add files via upload

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,21 @@
 import React from 'react';
 //import {useState} from 'react';
 import Layout from '../../components/Layout/Layout.js';
+import Header from '../../components/Header/Header.js';
 import pic from '../../assets/images/welcome.jpg';
+import home from './Home.css';
 
 const Home = () => {
+  const title="Home";
  //const [click, setClick] = useState(0)
   return (
-        <Layout> 
-        <div>
+        <div className="Home">
+        <Header newTitle={title} />
+        <Layout>
         <div>Hi, welcome to this page!</div>
         <div><img src={pic} /></div>
+        </Layout> 
         </div>
-        </Layout>
 
         //<p>You have clicked {click} times.</p>
         //<button onClick = {() => setClick(click + 1)}>Increment</button>
